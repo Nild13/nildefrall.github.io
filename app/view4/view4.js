03dee21fb0e21089a80692d1fae4fdbcd81e377e
oc <oskar.cejman@nxo.pl>
2021-03-18 09:00:27 +0100

    dunno

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -20,6 +20,7 @@ angular.module('myApp.view4', ['ngRoute'])
           let NFdataFromInput = { 'name': NFname, 'surname': NFsurname, 'PESEL':NFPESEL, 'confession': NFconfession, 'FavoritSport': NFUKsport, 'address': NFaddress, 'city': NFcity, 'voivodeship': NFvoivodeship};
           if (localStorage.getItem('NFdata') == null) {
             localStorage.setItem('NFdata', '[]');
+            console.log(NFdataFromInput.name)
           }
           localStorage.setItem('NFdata', JSON.stringify(NFdataFromInput));
           //<-------------------------------------------------------------->
@@ -27,15 +28,15 @@ angular.module('myApp.view4', ['ngRoute'])
           //<-------------------------------------------------------------->
           $scope.downloadDataFromLS()
     }
-    // cos ze splite, usun zawartosc i dodaj nowa to powinno rozwaizac problem :D jak wpiszesz we wszystkie inputy to sie wszystko zmienia pieknie, a jak tylko w jeden to nic
   }
-  $scope.NFdataTab = [];
+  
           $scope.downloadDataFromLS = function () {
           if (localStorage.getItem('NFdata') !== null) {
-            $scope.NFdataTab = JSON.parse(localStorage.getItem('NFdata'));
-            console.log("Imie" +' '+ $scope.NFdataTab.name)
+            $scope.NFdataTab = JSON.parse(localStorage.getItem('NFdata'))
+            console.log("Imie" +' '+ $scope.NFdataTab)
           }
   }
+  
   $scope.downloadDataFromLS()
           
   }]);

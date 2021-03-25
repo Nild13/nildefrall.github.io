60557a1012d7e0da5ea94c0fd0f21e5c1ee4384b
oc <oskar.cejman@nxo.pl>
2021-03-25 13:03:30 +0100

    try to fix PESEL issue

--- app/view4_2/view4_2.js
+++ app/view4_2/view4_2.js
@@ -11,6 +11,31 @@ angular.module('myApp.view4_2', ['ngRoute'])
 
 
   .controller('View4_2Ctrl', ['$scope', 'localStorageService', function ($scope, localStorageService){
+    $scope.metoda = function(){
+      if($scope.NFdataTab.PESEL === true){
+        return true
+      }
+    }
+    
+    $scope.validatePesel = function(pesel) {
+      let reg = /^[0-9]{11}$/;
+      if(reg.test(pesel) == false) 
+      return false;
+      else
+      {
+          let digits = (""+pesel).split("");
+          if ((parseInt(pesel.substring( 4, 6)) > 31)||(parseInt(pesel.substring( 2, 4)) > 12))
+          return false;
+           
+          let checksum = (1*parseInt(digits[0]) + 3*parseInt(digits[1]) + 7*parseInt(digits[2]) + 9*parseInt(digits[3]) + 1*parseInt(digits[4]) + 3*parseInt(digits[5]) + 7*parseInt(digits[6]) + 9*parseInt(digits[7]) + 1*parseInt(digits[8]) + 3*parseInt(digits[9]))%10;
+          if(checksum==0) checksum = 10;
+              checksum = 10 - checksum;
+          return (parseInt(digits[10])==checksum);
+      }
+      
+  }
+  
+  
     $scope.clearLS2 = function(){
      
       // if(confirm("Usunięcie spowoduje, że jeżeli rozpocząłeś pracę nad formularzem zostanie on wyczyszczony. Na pewno tego chcesz?")){
@@ -63,5 +88,6 @@ angular.module('myApp.view4_2', ['ngRoute'])
   }
   
   $scope.downloadDataFromLS()
+  // $scope.validatePesel($scope.NFdataTab.PESEL)
           
   }]);
